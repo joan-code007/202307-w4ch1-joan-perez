012f5a06cc9f1f83e00eee4b3ba7915a743e846b
Joan Pérez Lapuente <joanperez@MacBook-Pro-de-Joan.local>
2023-08-01 01:45:58 +0200

    Modify app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,13 @@
 const App = (): React.ReactElement => {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <span className="set--questions"></span>
+      <div className="set--answers">
+        <button type="submit"></button>
+        <button type="submit"></button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
